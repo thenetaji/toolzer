ca89e9e020e800aa6c0c13f7633d59db8e6a1e5b
Himesh <contact.dry528@passinbox.com>
2025-06-15 09:00:15 +0000

    add gtag fixed

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -67,7 +67,7 @@ export default function Document() {
             src="https://www.googletagmanager.com/ns.html?id=GTM-PT28VDL2"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
         {/** End Google Tag Manager (noscript) **/}
