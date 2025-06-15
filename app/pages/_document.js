4400af44875c6a11bdefe07d359368cc64cc80af
Himesh <contact.dry528@passinbox.com>
2025-06-15 08:29:45 +0000

    add gtag manager

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -4,6 +4,7 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
+      {/*favicons */}
       <Head>
         <link
           rel="icon"
@@ -36,24 +37,22 @@ export default function Document() {
         {/* Yandex */}
         <meta name="yandex-verification" content="b5c3a28f16fc141a" />
 
-        {/* Google Analytics */}
+        {/** Google Tag Manager */}
         <Script
           strategy="worker"
-          id="tag"
-          src="https://www.googletagmanager.com/gtag/js?id=G-V8DHJ6KM0Y"
-        ></Script>
-        <Script
-          strategy="worker"
-          id="window-layer"
+          id="gtm-init"
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag("js", new Date());
-              gtag("config", "G-V8DHJ6KM0Y");
-            `,
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-PT28VDL2');
+              `,
           }}
         />
+
+        {/**Ahref */}
         <Script
           strategy="worker"
           src="https://analytics.ahrefs.com/analytics.js"
@@ -62,6 +61,16 @@ export default function Document() {
         ></Script>
       </Head>
       <body>
+        {/** Google Tag Manager (noscript) **/}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PT28VDL2"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+        {/** End Google Tag Manager (noscript) **/}
         <Main />
         <NextScript />
       </body>
